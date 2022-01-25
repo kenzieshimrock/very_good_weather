20776e1957d8fd271d62c065028fad38bcba39b7
Kenzie Shimrock <kenzieshimrock@gmail.com>
2022-01-25 15:04:27 -0500

    Update populated_test.dart

--- test/weather/widgets/populated_test.dart
+++ test/weather/widgets/populated_test.dart
@@ -1,17 +1,16 @@
 // ignore_for_file: prefer_const_constructors
 
-
-void main() {
-  // group('WeatherPopulated', () {
-  //   final weather = Weather(
-  //     condition: WeatherCondition.clear,
-  //     temperature: Temperature(value: 42),
-  //     location: 'Chicago',
-  //     lastUpdated: DateTime(2020),
-  //   );
-
-
-  //   // Want to test for proper images being populated in the weather populated page.
-  //   // todo: research testing asset images.
-  // });
-}
+// void main() {
+//   // group('WeatherPopulated', () {
+//   //   final weather = Weather(
+//   //     condition: WeatherCondition.clear,
+//   //     temperature: Temperature(value: 42),
+//   //     location: 'Chicago',
+//   //     lastUpdated: DateTime(2020),
+//   //   );
+//
+//
+//   //   // Want to test for proper images being populated in the weather populated page.
+//   //   // todo: research testing asset images.
+//   // });
+// }
